ea5d2d8cdebde89d251001de5b1b9984ec9de332
dsitien <duongsitien123@gmail.com>
2023-10-15 12:12:49 +0700

    update pom

--- src/main/java/data/DBContext.java
+++ src/main/java/data/DBContext.java
@@ -18,7 +18,7 @@ public class DBContext {
 
     public DBContext() {
         try {
-            String url = "jdbc:sqlserver://localhost:1433;databaseName=CampusMunchies";
+            String url = "jdbc:sqlserver://localhost:1433;databaseName=CampusMunchies;encrypt=false;trustServerCertificate=true;";
             String username = "sa";
             String password = "123456";
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
