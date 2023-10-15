caf7c617b545b733c58defb3ace1657df2be2e8c
dsitien <duongsitien123@gmail.com>
2023-10-15 12:58:00 +0700

    Upload SQL 2rd

--- src/main/java/data/CustomerDAO.java
+++ src/main/java/data/CustomerDAO.java
@@ -4,10 +4,32 @@
  */
 package data;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 /**
  *
  * @author khang
  */
-public class CustomerDAO {
+public class CustomerDAO extends DBContext{
+    public void getAllCustomers() {
+        try {
+            String sql = "select * from Customer";
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                System.out.println(rs.getString(1));
+                System.out.println(rs.getString(2));
+                System.out.println(rs.getString(3));
+            }
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+    }
     
+    public static void main(String[] args) {
+       CustomerDAO cus = new CustomerDAO();
+       cus.getAllCustomers();
+    }
 }
--- src/main/java/data/EmployeeDAO.java
+++ src/main/java/data/EmployeeDAO.java
@@ -4,10 +4,32 @@
  */
 package data;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 /**
  *
  * @author khang
  */
-public class EmployeeDAO {
+public class EmployeeDAO extends DBContext{
+    public void getAllEmployees() {
+        try {
+            String sql = "select * from Employee";
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                System.out.println(rs.getString(1));
+                System.out.println(rs.getString(2));
+                System.out.println(rs.getString(3));
+            }
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+    }
     
+    public static void main(String[] args) {
+        EmployeeDAO f = new EmployeeDAO();
+        f.getAllEmployees();
+    }
 }
