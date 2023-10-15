6ff77cd914eec71d9fa02bfe024da2fb31f0553e
Khang Minh Ta <khangta67@gmail.com>
2023-10-15 13:47:50 +0700

    Update CustomerDAO

--- src/main/java/data/CustomerDAO.java
+++ src/main/java/data/CustomerDAO.java
@@ -4,10 +4,65 @@
  */
 package data;
 
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+
 /**
  *
  * @author khang
  */
-public class CustomerDAO {
-    
+public class CustomerDAO extends DBContext {
+
+    /**
+     * To check the customer's phone number and password if it exists or not
+     *
+     * @param phone
+     * @param password
+     * @return true if the account is found, false if the account is not found
+     */
+    public boolean isCustomerExisted(String phone, String password) {
+        try {
+            String sql = "select * from Customer "
+                    + "where cus_phone = '" + phone + "' and cus_password = CONVERT(VARCHAR(20), HASHBYTES('MD5', '" + password + "'), 2)";
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            return rs.next();
+        } catch (SQLException e) {
+            System.out.println(e);
+            return false;
+        }
+    }
+
+    /**
+     * Add new customer
+     *
+     * @param phone
+     * @param name
+     * @param password
+     * @param address
+     * @param birthday
+     * @param cancel
+     */
+    public void addCustomer(String phone, String name, String password, String address, Date birthday, int cancel) {
+        try {
+            String sql = "INSERT INTO Customer VALUES\n"
+                    + "(?, ?, CONVERT(VARCHAR(20), HASHBYTES('MD5', ?), 2), ?, ?, ?)";
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setString(1, phone);
+            ps.setString(2, name);
+            ps.setString(3, password);
+            ps.setString(4, address);
+            ps.setDate(5, birthday);
+            ps.setInt(6, cancel);
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+    }
+
+
+
 }
