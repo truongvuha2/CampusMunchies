fbab94987cc5791062ffde5f35660f6acd6f49af
Khang Minh Ta <khangta67@gmail.com>
2023-10-20 11:00:18 +0700

    Fix Customer Employee

---
+++ CMGuestCustomer/src/main/java/controller/guest/GuestSearch.java
@@ -0,0 +1,130 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package controller.guest;
+
+import dao.FoodDAO;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.util.List;
+import model.Food;
+
+/**
+ *
+ * @author khang
+ */
+public class GuestSearch extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try ( PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet GuestSearch</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet GuestSearch at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        FoodDAO f = new FoodDAO();
+        String name = request.getParameter("txt");
+        List<Food> foods = f.searchByName(name);
+        PrintWriter out = response.getWriter();
+        if (foods.size() != 0) {
+            for (Food food : foods) {
+                String b = "";
+                if (food.getSale() > 0) {
+                    b = "<p>$" + food.getRealPrice() + "<del>$" + food.getPrice() + "</del></p>";
+                } else {
+                    b = "<p>$" + food.getPrice() + "<p>";
+                }
+                out.println("<div class=\"food-item col-lg-3 col-md-4 col-sm-6 col-12\">\n"
+                        + "                            <div class=\"food-border\">\n"
+                        + "                                <div class=\"food-image col-lg-12 col-md-12 col-sm-12 col-4\">\n"
+                        + "                                    <img src=\"" + food.getImg() + "\" alt=\"\" srcset=\"\">\n"
+                        + "                                </div>\n"
+                        + "                                <div class=\"food-details col-lg-12 col-md-12 col-sm-12 col-8\">\n"
+                        + "                                    <p class=\"name\">" + food.getName() + "</p>\n"
+                        + "                                    <p class=\"price\">" + b + "</p>\n"
+                        + "                                    <div class=\"food-button\">\n"
+                        + "                                        <button class=\"cart\" onclick=\"\">\n"
+                        + "                                            <i class=\"fas fa-shopping-cart cart-icon\"></i>\n"
+                        + "                                        </button>\n"
+                        + "                                        <button class=\"detail\" onclick=\"getDetail('" + food.getId() + "')\">\n"
+                        + "                                            <i class=\"fas fa-info-circle\"></i>\n"
+                        + "                                        </button>\n"
+                        + "                                    </div>\n"
+                        + "                                </div>\n"
+                        + "                            </div>\n"
+                        + "                        </div>"
+                );
+            }
+        } else {
+            out.println("<style>.centered-text {\n"
+                    + "  display: flex;\n"
+                    + "  justify-content: center;\n"
+                    + "  align-items: center; \n"
+                    + "  height: 60vh;\n"
+                    + "text-align:center"
+                    + "} </style>"
+                    + "<h1 class=\"centered-text col-12\">No food is found<h1>");
+        }
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ CMGuestCustomer/src/main/webapp/guest/header.jsp
@@ -0,0 +1,170 @@
+<%-- 
+    Document   : home.jsp
+    Created on : Oct 12, 2023, 11:06:55 AM
+    Author     : khang
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <title>Header Example</title>
+
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css"
+              integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA=="
+              crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <style>
+            * {
+                box-sizing: border-box;
+                margin: 0;
+                padding: 0;
+            }
+
+
+            body {
+                font-family: "Poppins", sans-serif;
+                --color1: #FFF;
+                --color2: rgba(48,47,47,0.85);
+
+            }
+
+            header{
+                position: fixed;
+                top: 0;
+                width: 100%; /* Đảm bảo thanh header chiếm toàn bộ chiều rộng của trang */
+                z-index: 999;
+            }
+            .nav-bar {
+                width: 100%;
+                display: flex;
+                justify-content: space-between;
+                align-items: center;
+                list-style: none;
+                position: relative;
+                background-color: var(--color2);
+                
+                padding: 12px 20px;
+                margin-bottom: 0;
+            }
+
+            .logo img {
+                width: 60px;
+            }
+
+            .menu {
+                display: flex;
+            }
+
+            .menu li {
+                padding-left: 30px;
+            }
+
+            .menu li a {
+                display: inline-block;
+                text-decoration: none;
+                color: var(--color1);
+                text-align: center;
+                transition: 0.15s ease-in-out;
+                position: relative;
+                text-transform: uppercase;
+            }
+
+            .menu li a::after {
+                content: "";
+                position: absolute;
+                bottom: 0;
+                left: 0;
+                width: 0;
+                height: 1px;
+                background-color: var(--color1);
+                transition: 0.15s ease-in-out;
+            }
+
+            .menu li a:hover:after {
+                width: 100%;
+            }
+
+            .open-menu,
+            .close-menu {
+                position: absolute;
+                color: var(--color1);
+                cursor: pointer;
+                font-size: 1.5rem;
+                display: none;
+            }
+
+            .open-menu {
+                top: 50%;
+                right: 20px;
+                transform: translateY(-50%);
+            }
+
+            .close-menu {
+                top: 20px;
+                right: 20px;
+            }
+
+            #check {
+                display: none;
+            }
+
+            @media(max-width: 610px) {
+                .menu {
+                    flex-direction: column;
+                    align-items: center;
+                    justify-content: center;
+                    width: 80%;
+                    height: 100vh;
+                    position: fixed;
+                    top: 0;
+                    right: -100%;
+                    z-index: 100;
+                    background-color: var(--color2);
+                    transition: all 0.2s ease-in-out;
+                }
+
+                .menu li {
+                    margin-top: 40px;
+                }
+
+                .menu li a {
+                    padding: 10px;
+                }
+
+                .open-menu,
+                .close-menu {
+                    display: block;
+                }
+
+                #check:checked~.menu {
+                    right: 0;
+                }
+            }
+        </style>
+    </head>
+
+    <body>
+        <header>
+            <nav>
+                <ul class="nav-bar">
+                    <li class="logo">
+                        <a href=""><img src="../img/logo.png" alt=""/></a>
+                    </li>
+                    <input type="checkbox" id="check">
+                    <span class="menu">
+                        <li><a href="/CampusMunchies/guest/home">Home</a></li>
+                        <li><a href="/CampusMunchies/guest/menu">Menu</a></li>
+                        <li><a href="">About Us</a></li>
+                        <li><a  style = "color:orange" href="/CampusMunchies/guest/login">Login</a></li>
+                        <label for="check" class="close-menu"><i class="fas fa-times"></i></label>
+                    </span>
+                    <label for="check" class="close-menu">
+                        <i class="fas fa-bars"></i>
+                    </label>
+                </ul>
+            </nav>
+        </header>
+    </body>
+
+</html>
